469dfddb1c285a722c278c5f895b678ed8082c1a
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-03-28 19:34:38 +0900

    Three関係のファイル追加

---
+++ development/project-name/assets/js/_devjs/src/Visual/Canvas.js
@@ -0,0 +1,29 @@
+/**
+ * fileOverview:
+ * Project:
+ * File: Canvas
+ * Date:
+ * Author:
+ */
+
+// import Loader from './Loader.js';
+// import Sec01Video from '../Display/UI/Sec01Video.js';
+
+export default class Canvas {
+
+  constructor() {
+
+  }
+
+  onEnd() {
+
+
+  }
+
+  setEvents() {
+
+
+
+  }
+
+}
