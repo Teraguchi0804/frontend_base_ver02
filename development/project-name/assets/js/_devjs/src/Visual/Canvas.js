824336fc0a20a2b96e485bec797d4018de97ee23
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-03-29 18:45:48 +0900

    メンバ関数作成中

--- development/project-name/assets/js/_devjs/src/Visual/Canvas.js
+++ development/project-name/assets/js/_devjs/src/Visual/Canvas.js
@@ -9,20 +9,62 @@
 // import Loader from './Loader.js';
 // import Sec01Video from '../Display/UI/Sec01Video.js';
 
+'use strict';
+
 export default class Canvas {
 
   constructor() {
 
+    this.camera = null;
+    this.renderer = null;
+    this.scene = null;
+
+    this.createCamera = this._createCamera.bind(this);
+    this.createRenderer = this._createRenderer.bind(this);
+    this.createScene = this._createScene.bind(this);
+
   }
 
-  onEnd() {
+  /**
+   * 初期化
+   */
+  init(){
 
+    this.createCamera();
+    this.createRenderer();
+    this.createScene();
+
+  }
+
+  /**
+   * カメラ作成
+   */
+  _createCamera(){
+
+  }
+
+  /**
+   * レンダラー作成
+   */
+  _createRenderer(){
+
+  }
+
+  /**
+   *　シーン作成
+   */
+  _createScene(){
+
+  }
+
+
+  onLoad(){
 
   }
 
   setEvents() {
 
-
+    $(window).on('load', this.onLoad.bind(this));
 
   }
 
